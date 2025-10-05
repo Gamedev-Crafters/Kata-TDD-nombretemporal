fd26a29c351738b5c526ed17cee033ca10a54a35
adrianriverof <adrianriverof@gmail.com>
2025-10-05 10:29:31 +0200

    refactor: delete unused tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,21 +1,12 @@
 namespace Tests;
 
 public class Tests {
-	[SetUp]
-	public void Setup() { }
-
-	[Test]
-	public void Test1() {
-		Assert.Pass();
-	}
-
 	[Test]
 	public void OneReturnsOne()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
 		Assert.AreEqual(fizzBuzz.Convert(1), 1);
 	}
-	
 }
 
 public class FizzBuzz

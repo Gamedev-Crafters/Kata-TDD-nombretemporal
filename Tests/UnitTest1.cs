49babca5b3a12c2e7c4154fe32f44b4ed79e8864
fdeluna <fernando.deluna.romero@gmail.com>
2025-10-05 11:16:38 +0200

    feat: multiple of 3 and 5 is converted to FizzBuzz

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -72,6 +72,11 @@ public class FizzBuzz
 {
 	public static string Convert(int number)
 	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			return "FizzBuzz";
+		}
+		
 		if (number % 3 == 0)
 		{
 			return "Fizz";

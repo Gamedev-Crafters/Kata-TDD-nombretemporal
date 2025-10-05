91caa9d7d4acd92526bfff1907abf498f5da92e9
fdeluna <fernando.deluna.romero@gmail.com>
2025-10-05 10:23:16 +0200

    !test: fizzbuzz one returns one must return one

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,4 +8,20 @@ public class Tests {
 	public void Test1() {
 		Assert.Pass();
 	}
+
+	[Test]
+	public void OneReturnsOne()
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		Assert.Equals(fizzBuzz.Convert(1), 1);
+	}
+	
+}
+
+public class FizzBuzz
+{
+	public int Convert(int i)
+	{
+		return 0;
+	}
 }

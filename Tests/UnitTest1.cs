d154d5d541469ae298046b7324e00c10438b92dd
fdeluna <fernando.deluna.romero@gmail.com>
2025-10-05 10:40:40 +0200

    !test: two should be converted to two

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,17 @@
 namespace Tests;
 
 public class Tests {
+	
 	[Test]
 	public void OneConvertsToOne()
 	{
 		Assert.That("1", Is.EqualTo(FizzBuzz.Convert(1)));
+	}
+
+	[Test]
+	public void TwoConvertsToTwo()
+	{
+		Assert.That("2", Is.EqualTo(FizzBuzz.Convert(2)));
 	}
 }
 
